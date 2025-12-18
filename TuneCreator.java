e362e760380df3c40413a4be7fe5d2bdcd1c338e
John <jkespinosa.25@yahoo.com>
2025-12-17 19:44:51 -0500

    Added Tune Creator 47-48

---
+++ TuneCreator.java
@@ -0,0 +1,17 @@
+
+public class TuneCreator
+{
+    // instance variables - replace the example below with your own
+    private MidiPlayer player;
+
+    
+    public TuneCreator(){
+        player = new MidiPlayer();
+    }
+
+    public void CreateAndPlay(){
+        
+    }
+    
+    
+}

2224220aeb95e03534fb7be7194b4062583417df
John <jkespinosa.25@yahoo.com>
2025-12-17 21:06:03 -0500

    done until 54

---
+++ __SHELL124.java
@@ -0,0 +1,10 @@
+
+public class __SHELL124 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("C:\\Users\\jkesp\\OneDrive\\Documents\\Vanier Programming 1\\Backup projects\\bluej-projects\\Chapter03\\midi-player-copy");
+final SimpleTune simpleTu1 = (SimpleTune)__bluej_runtime_scope.get("simpleTu1");
+
+
+simpleTu1.showNotes();
+
+}}
